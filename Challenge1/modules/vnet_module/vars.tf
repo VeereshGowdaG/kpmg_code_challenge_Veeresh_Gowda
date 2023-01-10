047998823284b41e3d73a337710e8fb929bae1de
VeereshGowdaG <118176650+VeereshGowdaG@users.noreply.github.com>
2023-01-10 11:34:44 +0530

    Add files via upload

---
+++ Challenge1/modules/vnet_module/vars.tf
@@ -0,0 +1,7 @@
+variable "resource_group" {}
+variable "location" {}
+variable "environment" {}
+variable "vnet_iprange" {}
+variable "websubnet_iprange" {}
+variable "appsubnet_iprange" {}
+variable "dbsubnet_iprange" {}
